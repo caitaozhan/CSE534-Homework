d67028ee3063f1f7013809d2dafdc471308b4540
Caitao <caitaozhan@gmail.com>
2018-04-22 09:42:15 -0700

    Minor progress

--- homework-3/distancevector.py
+++ homework-3/distancevector.py
@@ -3,7 +3,31 @@ from collections import namedtuple
 class DistanceVector:
     '''This class encapsulates the distance vector
     '''
-    Distance = namedtuple('Distance', ['dest', 'cost', 'next'])
+    Distance = namedtuple('Distance', ['Dest', 'Cost', 'Next'])
 
-    def __init__(self):
-        pass
+    def __init__(self, hostname):
+        '''
+            Read neighbor file and init the distance vector
+        '''
+
+        self._dv = []  # a distance vector is essentially a list
+
+        with open('neighbor/' + hostname, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split(' ')
+                neighbor = line[0]
+                weight   = line[1]
+                self._dv.append(self.Distance(Dest=neighbor, Cost=weight, Next=neighbor))
+
+
+    def __str__(self):
+        string = ''
+        for distance in self._dv:
+            string += str(distance) + '\n'
+        return string
+
+
+if __name__ == '__main__':
+    dv = DistanceVector('r1')
+    print(dv)
--- homework-3/riplite.py
+++ homework-3/riplite.py
@@ -2,9 +2,35 @@ from distancevector import DistanceVector
 import sys
 
 
+class Host:
+    '''
+        This class encapsulate a host (host + router)
+    Attr:
+        hostname (str)
+        my_dv    (DistanceVector): this node's DistanceVector
+        other_dv (list): a list of DistanceVector of all other nodes
+    '''
+
+    all_hosts = ['h1', 'h2', 'r1', 'r2', 'r3', 'r4']
+
+    def __init__(self, hostname):
+        self.hostname = hostname
+        self.my_dv = DistanceVector(hostname)
+        self.other_dv = []
+        // DOTO: finish initialization
+
+    def __str__(self):
+        string = self.hostname + "'s distance vector: \n"
+        string += self.my_dv.__str__()
+        return string
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        host = sys.argv[1]
+        hostname = sys.argv[1]
+        host = Host(hostname)
         print(host)
     else:
         print('Error with parameters')
+
+
